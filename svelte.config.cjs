f0d1ee996c4ed021d61bc4aae0dbe3be4aa4996d
harsh <mangalamharsh78606@gmail.com>
2021-04-25 21:51:25 +0530

    node deploy

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,5 +1,5 @@
 const sveltePreprocess = require("svelte-preprocess");
-const node = require("@sveltejs/adapter-node");
+const adapter = require("@sveltejs/adapter-node");
 const pkg = require("./package.json");
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -15,8 +15,7 @@ module.exports = {
     // By default, `npm run build` will create a standard Node app.
     // You can create optimized builds for different platforms by
     // specifying a different adapter
-    adapter: node(),
-  
+    adapter: adapter(),
 
     // hydrate the <div id="svelte"> element in src/app.html
     target: "#svelte",
